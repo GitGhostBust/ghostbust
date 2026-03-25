8623556a51c8d4ce9f47ad2173966f58b0d62476
GitGhostBust <ghostbustofficial@gmail.com>
2026-03-25 03:17:38 -0700

    feat: add sentryVitePlugin and sourcemap: true to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()], build: { rollupOptions: { input: { main: './index.html', app: './app.html', profile: './profile.html', community: './community.html', tos: './tos.html', privacy: './privacy.html', score: './score.html' } } } })
+import { sentryVitePlugin } from '@sentry/vite-plugin'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      sourcemaps: {
+        deleteAfterUpload: true,
+      },
+    }),
+  ],
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        app: './app.html',
+        profile: './profile.html',
+        community: './community.html',
+        tos: './tos.html',
+        privacy: './privacy.html',
+        score: './score.html',
+      }
+    }
+  }
+})
